bff93cc39e0dafbb3a7f82b43fbaf089da3a4ebc
Eduardo Carneiro <eduuxx94@gmail.com>
2023-10-19 16:32:43 +0100

    fix norminette

--- src/core/objects/map/map_render.c
+++ src/core/objects/map/map_render.c
@@ -1,11 +1,11 @@
 #include "Map.h"
 
-int RemapXToScreen(t_map *map, int x_map_position)
+int remap_x_screen(t_map *map, int x_map_position)
 {
     return (x_map_position + (-map->min_x)) / map->scale;
 }
 
-int RemapYToScreen(t_map *map, int y_map_position, SDL_Renderer *renderer)
+int remap_y_screen(t_map *map, int y_map_position, SDL_Renderer *renderer)
 {
     int i_render_x;
     int i_render_y;
@@ -36,10 +36,10 @@ void __render_map_wall(t_map *this, SDL_Renderer *renderer, int iXShift, int iYS
         t_vertex *vStart = (t_vertex *)vertexes[((t_linedef *)linedefs[i].value)->start_vertex].value; // Read the first point
         t_vertex *vEnd = (t_vertex *)vertexes[((t_linedef *)linedefs[i].value)->end_vertex].value;     // Read the second point
         SDL_RenderDrawLine(renderer,
-                           RemapXToScreen(this, vStart->x_position),
-                           RemapYToScreen(this, vStart->y_position, renderer),
-                           RemapXToScreen(this, vEnd->x_position),
-                           RemapYToScreen(this, vEnd->y_position, renderer));
+                           remap_x_screen(this, vStart->x_position),
+                           remap_y_screen(this, vStart->y_position, renderer),
+                           remap_x_screen(this, vEnd->x_position),
+                           remap_y_screen(this, vEnd->y_position, renderer));
         i++;
     }
 }
@@ -58,8 +58,8 @@ void __render_map_wall_player(t_map *this, SDL_Renderer *renderer, int iXShift, 
     for (int i = 0; i < 9; ++i)
     {
         SDL_RenderDrawPoint(renderer,
-                            RemapXToScreen(this, this->player.x_position) + this->direction[i][0],
-                            (RemapYToScreen(this, this->player.y_position, renderer) + this->direction[i][1]));
+                            remap_x_screen(this, this->player.x_position) + this->direction[i][0],
+                            (remap_y_screen(this, this->player.y_position, renderer) + this->direction[i][1]));
     }
 }
 
@@ -68,16 +68,16 @@ void __render_auto_map_node(t_map *this, SDL_Renderer *renderer, int node_id)
     t_wad_node *node = array(this->nodes)->get(node_id);
 
     SDL_Rect RightRect = {
-        RemapXToScreen(this, node->right_box_left),
-        RemapYToScreen(this, node->right_box_top, renderer),
-        RemapXToScreen(this, node->right_box_right) - RemapXToScreen(this, node->right_box_left) + 1,
-        RemapYToScreen(this, node->right_box_bottom, renderer) - RemapYToScreen(this, node->right_box_top, renderer) + 1};
+        remap_x_screen(this, node->right_box_left),
+        remap_y_screen(this, node->right_box_top, renderer),
+        remap_x_screen(this, node->right_box_right) - remap_x_screen(this, node->right_box_left) + 1,
+        remap_y_screen(this, node->right_box_bottom, renderer) - remap_y_screen(this, node->right_box_top, renderer) + 1};
 
     SDL_Rect LeftRect = {
-        RemapXToScreen(this, node->left_box_left),
-        RemapYToScreen(this, node->left_box_top, renderer),
-        RemapXToScreen(this, node->left_box_right) - RemapXToScreen(this, node->left_box_left) + 1,
-        RemapYToScreen(this, node->left_box_bottom, renderer) - RemapYToScreen(this, node->left_box_top, renderer) + 1};
+        remap_x_screen(this, node->left_box_left),
+        remap_y_screen(this, node->left_box_top, renderer),
+        remap_x_screen(this, node->left_box_right) - remap_x_screen(this, node->left_box_left) + 1,
+        remap_y_screen(this, node->left_box_bottom, renderer) - remap_y_screen(this, node->left_box_top, renderer) + 1};
 
     SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
     SDL_RenderDrawRect(renderer, &RightRect);
@@ -86,14 +86,14 @@ void __render_auto_map_node(t_map *this, SDL_Renderer *renderer, int node_id)
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
     SDL_RenderDrawLine(renderer,
-                       RemapXToScreen(this, node->x_partition),
-                       RemapYToScreen(this, node->y_partition, renderer),
-                       RemapXToScreen(this, node->x_partition + node->change_x_partition),
-                       RemapYToScreen(this, node->y_partition + node->change_y_partition, renderer));
+                       remap_x_screen(this, node->x_partition),
+                       remap_y_screen(this, node->y_partition, renderer),
+                       remap_x_screen(this, node->x_partition + node->change_x_partition),
+                       remap_y_screen(this, node->y_partition + node->change_y_partition, renderer));
     // printf("x_partition: %i y_partition: %i\n", node->change_x_partition, node->change_y_partition);
 }
 
-bool IsPointOnLeftSide(t_map *map, int XPosition, int YPosition, int iNodeID)
+bool is_on_left_side(t_map *map, int XPosition, int YPosition, int iNodeID)
 {
     // int dx = XPosition - m_Nodes[iNodeID].XPartition; // Find X the point relative to the player
     int dx = XPosition - ((t_wad_node *)array(map->nodes)->get(iNodeID))->x_partition; // Find X the point relative to the player
@@ -125,7 +125,7 @@ void render_bsp_nodes(t_map *this, SDL_Renderer *renderer, int node_id)
     // __render_auto_map_node(this, renderer, node_id);
     // SDL_RenderPresent(renderer);
     // SDL_Delay(500);
-    bool isOnLeft = IsPointOnLeftSide(this, this->player.x_position, this->player.y_position, node_id);
+    bool isOnLeft = is_on_left_side(this, this->player.x_position, this->player.y_position, node_id);
 
     if (isOnLeft)
     {
