b8c256c2d3115a2743b9b945ce00b6ac06ceeffd
Eduardo Carneiro <eduuxx94@gmail.com>
2023-10-30 13:24:51 +0000

    added viewRenderer header to project

---
+++ headers/ViewRenderer.h
@@ -0,0 +1,36 @@
+#ifndef VIEW_RENDERER_H
+#define VIEW_RENDERER_H
+
+#include "Engine.h"
+
+typedef struct s_view_renderer t_view_renderer;
+
+struct s_view_renderer
+{
+    int render_x_size;
+    int render_y_size;
+    int auto_map_scale_factor;
+
+    t_map *map;
+    t_player *player;
+
+    SDL_Renderer *m_pRenderer;
+};
+
+//functions:
+void init(t_map *map, t_player *player);
+void render(bool is_automap);
+void add_wall_in_fOV(t_wad_seg seg, float V1Angle, float V2Angle);
+void init_frame();
+void set_draw_color(int r, int g, int b);
+void draw_rect(int x1, int y1, int x2, int y2);
+void draw_line(int x1, int y1, int x2, int y2);
+
+void render_auto_map();
+void render_3d_view();
+
+int angle_to_screen(float angle);
+int remap_x_to_screen(int x_map_position);
+int remap_y_to_screen(int y_map_position);
+
+#endif
